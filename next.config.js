735e735ef67361953403a770dd393cf1c5b53e0b
Ahmad Alyajouri <ahmad@Ahmads-MacBook-Pro.local>
2023-04-26 11:30:59 -0700

    disable cache on home page

--- next.config.js
+++ next.config.js
@@ -19,6 +19,17 @@ const nextConfig = {
       },
     ]
   }, */
+  headers: () => [
+    {
+      source: '/',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-store',
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = nextConfig;
